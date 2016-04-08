329c2e53b1b2c9de312a25cf402e70b4993a0925
chris marlowe <chris@codechris.com>
2016-04-08 05:23:26 -0500

    1st and 2nd activity toggle completed

--- activities/src/main/java/com/delta/activities/SecondActivity.java
+++ activities/src/main/java/com/delta/activities/SecondActivity.java
@@ -1,35 +1,31 @@
 package com.delta.activities;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 
-public class FirstActivity extends Activity {
+/**
+ * Created by Christopher on 4/7/2016.
+ */
+public class SecondActivity extends Activity {
 
     private Button mButton = null;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_second);
 
-        mButton = (Button) findViewById(R.id.goSecondActivity);
-
+        mButton = (Button) findViewById(R.id.goFirstActivty);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), SecondActivity.class);
+                Intent i = new Intent(v.getContext(), FirstActivity.class);
                 startActivity(i);
 
             }
@@ -38,7 +34,7 @@ public class FirstActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.first, menu);
         return true;
@@ -55,5 +51,4 @@ public class FirstActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
